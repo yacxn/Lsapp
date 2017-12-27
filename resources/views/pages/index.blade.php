5b7c8ce2d8eda9e6c9b4143de7d1eabbbf26fe38
Your Name <yacinebenz3@gmail.com>
2017-12-27 15:51:45 +0000

    connected model

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,6 +3,6 @@
     <div class="jumbotron text-center">
     <h1>Welcome</h1>
     <p>This the Laravel application by Yacine !</p>
-    <p><a href="#" class="btn btn-primary btn-lg" role="button">Login</a></p>
+    <p><a href="#" class="btn btn-primary btn-lg" role="button">Login</a> <a href="#" class="btn btn-success btn-lg" role="button">Register</a></p>
   </div>
 @endsection
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app');
+@section('content')
+    <h3>Posts</h3>
+@endsection
